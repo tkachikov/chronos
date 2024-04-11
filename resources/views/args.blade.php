8f651b1d00e688b6b39a9748f9de2d39b02d9b7e
tkachikov <a.tkachikov@pusk.ru>
2024-04-11 16:19:12 +0300

    changed description tooltip attributes, added required

--- resources/views/args.blade.php
+++ resources/views/args.blade.php
@@ -3,23 +3,50 @@
         @php
             $name = $prefix.$input->getName();
             $description = $input->getDescription();
+            $descriptionAttributes = '';
+            if ($description) {
+                $descriptionAttributes = [
+                    'data-bs-toggle' => 'tooltip',
+                    'data-bs-title' => $description,
+                    'data-bs-placement' => 'top',
+                ];
+                $descriptionAttributes = implode(
+                    ' ',
+                    array_map(
+                        fn ($v, $k) => "$k=\"$v\"",
+                        $descriptionAttributes,
+                        array_keys($descriptionAttributes),
+                    ),
+                );
+            }
         @endphp
         <div class="row w-100 mx-auto align-items-center">
             <div class="col">
-                @if($description)
-                    <span data-bs-toggle="tooltip" data-bs-title="{{ $description }}" data-bs-placement="top">
-                        {{ $name }}
-                    </span>
-                @else
+                <span {!! $descriptionAttributes !!} class="d-inline-block">
+                    @if($input->isRequired())
+                        <span class="position-absolute translate-middle p-1 bg-danger border border-light rounded-circle"></span>
+                    @endif
                     {{ $name }}
-                @endif
+                </span>
             </div>
-            <div class="col">
+            <div class="col my-2">
                 @if($key === 'arguments' || ($key === 'options' && $input->acceptValue()))
-                    <input type="text" name="args[{{ $name }}]" class="form-control" value="{{ $schedule?->args[$name] ?? $input->getDefault() }}" form="{{ $form }}">
+                    <input type="text"
+                           name="args[{{ $name }}]"
+                           class="form-control"
+                           value="{{ $schedule?->args[$name] ?? $input->getDefault() }}"
+                           form="{{ $form }}"
+                           @required($input->isRequired())
+                    />
                 @else
                     <div class="form-check form-switch">
-                        <input class="form-check-input" type="checkbox" role="switch" name="args[{{ $name }}]" form="{{ $form }}">
+                        <input class="form-check-input"
+                               type="checkbox"
+                               role="switch"
+                               name="args[{{ $name }}]"
+                               form="{{ $form }}"
+                               @required($input->isRequired())
+                        />
                     </div>
                 @endif
             </div>
