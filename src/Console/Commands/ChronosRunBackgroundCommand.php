9b7d3cfc19af02c096490576ab8db77121a8d536
tkachikov <a.tkachikov@pusk.ru>
2024-04-15 16:18:35 +0300

    changed run in real time without jobs

---
+++ src/Console/Commands/ChronosRunBackgroundCommand.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tkachikov\Chronos\Console\Commands;
+
+use Illuminate\Console\Command;
+use Tkachikov\Chronos\Services\CommandRunService;
+use Tkachikov\Chronos\Traits\ChronosRunnerTrait;
+use Illuminate\Support\Facades\Log;
+use Tkachikov\Chronos\Models\Command as CommandModel;
+
+class ChronosRunBackgroundCommand extends Command
+{
+    use ChronosRunnerTrait;
+
+    protected $signature = 'chronos:run-background {uuid}';
+
+    protected $description = 'Command for run other commands in background';
+
+    public function handle(CommandRunService $service): int
+    {
+        $data = cache()->get($this->argument('uuid'));
+        $command = CommandModel::findOrFail($data['command_id']);
+        $service->run($command, $data['uuid'], $data['args']);
+
+        return self::SUCCESS;
+    }
+}
