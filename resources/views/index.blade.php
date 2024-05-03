55f61f415c5c59d48e839a001ae234fd00d06563
tkachikov <a.tkachikov@pusk.ru>
2024-05-03 17:16:26 +0300

    added link to app main page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,11 +1,13 @@
 @extends('chronos::layout', ['title' => 'test'])
 @section('content')
-    <div class="row w-100 mx-auto mb-3">
-        <div class="col px-0">
-            <a class="btn btn-link text-decoration-none" href="{{ route('chronos.main') }}">
-                <h1 class="h1 m-0">Commands</h1>
-            </a>
-        </div>
+    <div class="d-flex flex-row mb-3">
+        <a class="btn btn-link text-decoration-none" href="/">
+            <h1 class="h1 m-0">{{ config('app.name') }}</h1>
+        </a>
+        <span class="h4 m-0 py-3 text-muted">/</span>
+        <a class="btn btn-link text-decoration-none" href="{{ route('chronos.main') }}">
+            <h1 class="h1 m-0">Commands</h1>
+        </a>
     </div>
     <div class="row w-100 mx-auto">
         <div class="col">
