c3994ae28c3a412c7b07106f88077bfabd4cc31d
tkachikov <a.tkachikov@pusk.ru>
2025-04-24 13:52:47 +0300

    fix view column "in code"

--- resources/views/schedules/table.blade.php
+++ resources/views/schedules/table.blade.php
@@ -64,15 +64,16 @@
                     </td>
                     <td @class($border)>
 <pre class="m-0">$schedule
-    ->command(
-        {{ $command->getShortName() . '::class'}}@if($item->preparedArgs),
-    [
+    ->command(@if($item->preparedArgs)&nbsp;
+        {{ $command->getShortName() . '::class'}},
+        [
     @foreach($item->preparedArgs as $key => $arg)
         '{{ $key }}' => {{ is_numeric($arg) ? $arg : (is_null($arg) ? 'null' : "'$arg'") }},
     @endforeach
-    @endif],
+    ],
     )
-    ->{{ $item->time_method }}({{ $item->time_params ? collect($item->time_params)->map(fn ($v) => "'$v'")->implode(', ') : '' }}){{
+    @else{{ $command->getShortName() . '::class' }})
+    @endif->{{ $item->time_method }}({{ $item->time_params ? collect($item->time_params)->map(fn ($v) => "'$v'")->implode(', ') : '' }}){{
     $item->without_overlapping ? "\r\n    ->withoutOverlapping(" . ($item->without_overlapping_time !== 1440 ? $item->without_overlapping_time : '') . ')' : ''
 }}{{ $item->run_in_background ? "\r\n    ->runInBackground()" : '' }};</pre>
                     </td>
