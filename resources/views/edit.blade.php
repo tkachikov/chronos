a7e92636969d806375bc725901080e3b103dbd49
tkachikov <a.tkachikov@pusk.ru>
2025-04-24 12:03:56 +0300

    fix length column "in code" in the editing command

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -389,7 +389,14 @@
                                         </td>
                                         <td @class($border)>
 <pre class="m-0">$schedule
-    ->command({{ $command->getShortName() . '::class' . ($args ? ", {$args}" : '') }})
+    ->command(
+        {{ $command->getShortName() . '::class'}}@if($item->preparedArgs),
+        [
+    @foreach($item->preparedArgs as $key => $arg)
+        '{{ $key }}' => {{ is_numeric($arg) ? $arg : (is_null($arg) ? 'null' : "'$arg'") }},
+    @endforeach
+    @endif],
+    )
     ->{{ $item->time_method }}({{ $item->time_params ? collect($item->time_params)->map(fn ($v) => "'$v'")->implode(', ') : '' }}){{
     $item->without_overlapping ? "\r\n    ->withoutOverlapping(" . ($item->without_overlapping_time !== 1440 ? $item->without_overlapping_time : '') . ')' : ''
 }}{{ $item->run_in_background ? "\r\n    ->runInBackground()" : '' }};</pre>
