dd8b2729e2b50993940f4e4e85c32a64ad49d5ee
tkachikov <a.tkachikov@pusk.ru>
2025-11-19 19:04:59 +0300

    added test for artisan repository and command manager

---
+++ tests/Feature/CommandManagerTest.php
@@ -0,0 +1,83 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Feature;
+
+use Illuminate\Support\Facades\Artisan;
+use Orchestra\Testbench\TestCase;
+use Tkachikov\Chronos\Helpers\DatabaseHelper;
+use Tkachikov\Chronos\Managers\CommandManager;
+use Tkachikov\Chronos\Models\Command;
+use Tkachikov\Chronos\Providers\ChronosServiceProvider;
+
+final class CommandManagerTest extends TestCase
+{
+    public function testGettingCommandsFromStorage(): void
+    {
+        $exists = $this
+            ->app
+            ->make(DatabaseHelper::class)
+            ->hasTable(Command::class);
+
+        $this->assertTrue($exists);
+        $this->assertEquals(0, Command::count());
+
+        $manager = $this
+            ->app
+            ->make(CommandManager::class);
+
+        $decorators = $manager->get();
+
+        $countCommands = Command::count();
+
+        $this->assertNotEquals(0, count($decorators));
+
+        foreach ($decorators as $decorator) {
+            $this->assertInstanceOf(Command::class, $decorator->getModel());
+        }
+
+        foreach (Artisan::all() as $command) {
+            $this->assertDatabaseHas(
+                (new Command)->getTable(),
+                ['class' => $command::class],
+            );
+        }
+
+        $this->assertEquals(0, $manager->getApps()->count());
+        $this->assertNotEquals(0, $manager->getSystems()->count());
+        $this->assertNotEquals(0, $manager->getChronos()->count());
+        $this->assertEquals($countCommands, Command::count());
+    }
+
+    protected function getPackageProviders($app): array
+    {
+        return [
+            ChronosServiceProvider::class,
+        ];
+    }
+
+    protected function getEnvironmentSetUp($app): void
+    {
+        $app['config']->set('database.default', 'testing');
+        $app['config']->set('database.connections.testing', [
+            'driver' => 'sqlite',
+            'database' => ':memory:',
+            'prefix' => '',
+        ]);
+    }
+
+    protected function defineDatabaseMigrations(): void
+    {
+        $this->loadLaravelMigrations();
+    }
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this
+            ->artisan('migrate')
+            ->run();
+    }
+}
